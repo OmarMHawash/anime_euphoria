2f14a5c6d67b654cbbb1741dc0fcd9547da78f70
asmaalasaada <asma.alsaada96@gmail.com>
2021-06-02 11:45:36 +0300

    update win

--- animelist/myapp/models.py
+++ animelist/myapp/models.py
@@ -5,6 +5,7 @@ class Users(models.Model):
     username = models.CharField(max_length=45)
     email = models.CharField(max_length=45)
     password = models.CharField(max_length=45)
+    role=models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -30,4 +31,3 @@ class Category(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-class Roles(models.Model):
