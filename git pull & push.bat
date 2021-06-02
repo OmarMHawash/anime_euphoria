f23d23552a6cb1f6cddbd134c24179f546085681
OmarMHawash <omarmhawash@gmail.com>
2021-06-02 11:06:45 +0300

    update omar

---
+++ git pull & push.bat
@@ -0,0 +1,6 @@
+@echo off
+git pull https://github.com/Huthifakhraishah/anime_euphoria.git
+git add .
+git commit -m "update win"
+git push origin master
+echo "pulled and pushed to repo successfully!"
--- git pull.bat
+++ git pull.bat
@@ -1,2 +1,3 @@
 @echo off 
 git pull https://github.com/Huthifakhraishah/anime_euphoria.git
+echo "Pulling completed!"
