dabe3c24cab97107ad7d396bb3f0f238a3cf2389
huthifa <huthifakhraishah98@gmail.com>
2021-06-02 11:11:05 +0300

    update huthifa

--- git pull & push.bat
+++ git pull & push.bat
@@ -1,6 +1,6 @@
 @echo off
 git pull https://github.com/Huthifakhraishah/anime_euphoria.git
 git add .
-git commit -m "update win"
+git commit -m "update huthifa"
 git push origin master
 echo "pulled and pushed to repo successfully!"
