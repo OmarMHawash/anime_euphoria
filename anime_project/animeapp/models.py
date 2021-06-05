bc264cc24b834547df789db318d5ef41d4f465e3
OmarMHawash <omarmhawash@gmail.com>
2021-06-05 11:08:13 +0300

    update omar

--- anime_project/animeapp/models.py
+++ anime_project/animeapp/models.py
@@ -1,10 +1,33 @@
 from django.db import models
 # from django.db.models.enums import Choices
 from user_app.models import Users
-from enum import Enum 
+from enum import Enum
 
-
-
+class AnimeManager(models.Manager):
+    def basic_anime(self, postData):
+        errors = {}
+        if len(postData['title']) <3:
+            errors["title"] = "Anime name should be at least 3 characters"
+        if len(postData['desc']) < 125 :
+            errors["desc"] = "password should be at least 125 characters"        
+        if postData['rating'] >= 0 and postData['rating'] <= 10 :
+            errors["rating"] = "Rating should be between 0 and 10"
+        if len(postData['pic_url']) > 5 :
+            errors["pic_url"] = "You should enter url "
+        if len(postData['trailer_url']) > 5 :
+            errors["trailer_url"] = "You should enter trailer url "    
+        if len(postData['pic_bg']) > 5 :
+            errors["pic_bg"] = "You should enter background url "  
+    def basic_comments(self, postData):
+        errors = {}
+        if len(postData['comment']) < 1:
+            errors["email"] = " email should be at least 1 characters"
+        return errors
+    def basic_category(self, postData):
+        errors = {}
+        if len(postData['category']) != "":
+            errors["category"] = "category not in list"
+        return errors
 
 class Animes(models.Model):
     title=models.CharField(max_length=45)
@@ -17,6 +40,7 @@ class Animes(models.Model):
     fav_list=models.ManyToManyField(Users,related_name="liked_a")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects =AnimeManager()  
 
 class Comments(models.Model):
     comment=models.TextField()
@@ -24,6 +48,7 @@ class Comments(models.Model):
     anime_id=models.ForeignKey(Animes, related_name="a_comment",on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects =AnimeManager()  
 
 class Category(models.Model):
     Action = 'AC'
@@ -54,3 +79,4 @@ class Category(models.Model):
     genre = models.ForeignKey(Animes,related_name="anime_category",on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects =AnimeManager()  
--- anime_project/user_app/models.py
+++ anime_project/user_app/models.py
@@ -1,6 +1,26 @@
 from django.db import models
 from animeapp.models import *
 import re 
+class UserManager(models.Manager):
+    def basic_reg(self, postData):
+        errors = {}
+        if len(postData['username']) <3:
+            errors["last_name"] = "last name should be at least 3 characters"
+        if len(postData['passwd']) < 8 :
+            errors["passwd"] = "password should be at least 8 characters"
+        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        if not EMAIL_REGEX.match(postData['email']):           
+            errors['email'] = "Invalid email address!"
+        if len(postData['email']) < 8:
+            errors["email"] = "email should be at least 8 characters"
+        return errors
+    def basic_log(self, postData):
+        errors = {}
+        if len(postData['email']) < 8:
+            errors["email"] = " email should be at least 8 characters"
+        if len(postData['passwd']) < 8:
+            errors["passwd"] = "password should be at least 8 characters"
+        return errors
 
 class Users(models.Model):
     username = models.CharField(max_length=45)
@@ -10,3 +30,4 @@ class Users(models.Model):
     role=models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = UserManager()
