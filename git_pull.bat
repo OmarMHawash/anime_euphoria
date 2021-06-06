fee2da97991547fa5143bdfe2df2694914fd6857
OmarMHawash <omarmhawash@gmail.com>
2021-06-06 16:05:41 +0300

    deploy

--- git_pull.bat
+++
@@ -1,3 +0,0 @@
-@echo off 
-git pull https://github.com/Huthifakhraishah/anime_euphoria.git
-echo "Pulling completed!"
--- git_pull_push.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-git pull https://github.com/Huthifakhraishah/anime_euphoria.git
-git add .
-git commit -m "update win"
-git push origin master
-echo "pulled and pushed to repo successfully!"
